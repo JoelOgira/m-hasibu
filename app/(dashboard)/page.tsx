e707ac67ef76be40000fa638be40934df156cf3e
Jay <joelogira@gmail.com>
2024-06-13 22:43:47 +0300

    updated the homepage

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,18 @@
-import { UserButton } from "@clerk/nextjs";
+"use client"
+
+import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
+  const { onOpen } = useNewAccount()
+
   return (
-    <main className="">
-      <UserButton afterSignOutUrl="/" />
+    <main className="min-h-100">
+      <Button
+        onClick={onOpen}
+      >
+        Add New Account
+      </Button>
     </main>
   )
 }
